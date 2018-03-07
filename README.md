6164d48ba976a97d1c97d23dfaefe7dfcfc1dd68
root <root@b104-raspberry.nswccd.navy.mil>
2018-03-06 19:46:25 -0800

    Added delay to turn off

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
-# Port Control. 
+# portctl.py Port Control. 
 
 Shutdown Ethernet ports on a switch in response to a detected 
-switch closure on GPIO pins. The file portctl_conf.json is the file that maps
-GPIO pins to Ethernet Switch IP and ports. See example portctl_conf.json.sample
-for an example. Remember JSON does not permit comments. 
+switch closure on GPIO pins on a Raspberry pi. 
+
+The file portctl_conf.json is the file that maps GPIO pins to 
+Ethernet Switch IP and ports. See example portctl_conf.json.sample
+for an example. Support multiple Ethernet Switches and Ports per
+GPIO pin. Remember JSON does not permit comments. 
+
+And example systemd unit file is provided. This is configured to 
+restart on failure. Modify as needed
+
+On startup the script kills any other instances running. This allows
+a restart in a cron. Only works on Linux as it used a system call 
+to get list of processes. 
+
+Supports only one optioin -v to print out more verbose output for 
+testing purposes. 
--- portctl.py
+++ portctl.py
@@ -1,46 +1,81 @@
 #!/usr/bin/python 
 
-# Turns off Ethernet ports in a switch when a alarm (switch closure detected via GPIO)
-# Port numbers and GPIO pins are defined in a json configuration file specified below
-# 
+# Turns off Ethernet ports when a alarm (switch closure detected via GPIO)
+# Port numbers and GPIO pins are defined in a json configuration file
+# When a switch closure is detected there is a configurable delay before turning off the ports
 
 from pysnmp.hlapi import *
 import json
-
-import RPi.GPIO as GPIO
 import time
+import os.path
+import signal
+import subprocess
+import sys
 
 import pprint
 pp = pprint.PrettyPrinter(indent=2)
 
+# Raspberry pi modules
+import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
+
+#----------------------------------    
+# Command Line Options... 
+#----------------------------------    
+import argparse
+parser = argparse.ArgumentParser(description='Ethernet Port Control')
+parser.add_argument("-v",help="Run with standard output",action="store_true")
+args = parser.parse_args()
+if args.v: print("Verbose Mode Selected")
 
 #----------------------------------    
 # Configuration... 
 #----------------------------------    
 conf_file = 'portctl_conf.json'
-loop_timer = 20
+Community = 'Bayview_SNMP_1234$'
+loop_timer = 10
+delay_off_timer = 300
 
 with open(conf_file) as data_file:  
     pconf = json.load(data_file)
 
+if args.v: pp.pprint(pconf)
+
 #----------------------------------    
 # Define Functions... 
 #----------------------------------    
+
+#-- Only one one instance of this script to run at a time 
+def stop_other_procs():
+   
+   plst = subprocess.Popen(['ps', '-A'], stdout=subprocess.PIPE)
+   out,err = plst.communicate()
+
+   pid_self = os.getpid()
+   for line in out.splitlines():
+      if os.path.basename(sys.argv[0]) in line:
+          parts = line.split()
+          if int(parts[0]) == pid_self: continue
+          os.kill(int(parts[0]), signal.SIGKILL)
+
+#-- Uses SNMP to turn Ether Ports on/off 
 def set_ports(pconf,onoff): 
     cmdint = {'off': 2, 'on':1 }
     
     for sw_set in pconf:
       
         for port_set in sw_set['ports']:
+
            print("Setting port {} => {}".format(port_set,cmdint[onoff]))
-           g = setCmd(SnmpEngine(),
-                      CommunityData('Bayview_SNMP_1234$', mpModel=1),
+
+           gen_rtn = setCmd(SnmpEngine(),
+                      CommunityData(Community, mpModel=1),
                       UdpTransportTarget((sw_set['ip'], 161)),
                       ContextData(),
                       ObjectType(ObjectIdentity('IF-MIB', 'ifAdminStatus', port_set),cmdint[onoff]))
 
-           errorIndication, errorStatus, errorIndex, varBinds = next(g)
+           # The snmp lib creates a 'generator' so use a next to get the return values 
+           errorIndication, errorStatus, errorIndex, varBinds = next(gen_rtn)
 
            if errorIndication: print(errorIndication)
            elif errorStatus: print('%s at %s' % (errorStatus.prettyPrint(),
@@ -53,17 +88,17 @@ def set_ports(pconf,onoff):
 #----------------------------------    
 state = {}
 
-print("Setup")
-print("---------")
+#-- stop other process so we can cron this once a day
+stop_other_procs()
+
+#-- setup the GPIO ports
 for pin in pconf:
     GPIO.setup(int(pin), GPIO.IN, pull_up_down=GPIO.PUD_UP)
     print("PIN {} configured as Input".format(pin))
     state[pin] = ''
-   
-print("---------\n")
 
 #----------------------------------    
-# Loop
+# Loop Forever...
 #----------------------------------    
 cnt=0
 while(1):
@@ -75,11 +110,17 @@ while(1):
             set_ports(pconf[pin],'on')
             state[pin] = 'on'
             
-        # Pin low (false) means the switch is close 
+        # Pin low (false) means the switch is closed 
         elif not GPIO.input(int(pin)) and state[pin] != 'off':
-            set_ports(pconf[pin],'off')
-            state[pin] = 'off'
+            
+           # We wait before turning ports off 
+           if args.v: print("Detected Closure on GPIO {}".format(pin))
+           print "Delaying {} Seconds before shutting down".format(delay_off_timer)
+           time.sleep(delay_off_timer)
+            
+           set_ports(pconf[pin],'off')
+           state[pin] = 'off'
     
-    print "loop {}".format(cnt)
+    if (args.v ): print "loop {}".format(cnt)
     time.sleep(loop_timer)
     cnt = cnt + 1
